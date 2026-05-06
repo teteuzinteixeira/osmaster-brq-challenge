d67b9dd7d388ed3b2e063299cf69d60fa03978fb
Mateus Teixeira <mateus.programacao29@gmail.com>
2026-05-06 00:19:30 -0300

    initial setup

---
+++ src/main/java/com/OSMaster/ServiceOsMasterApplication.java
@@ -0,0 +1,13 @@
+package com.OSMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceOsMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceOsMasterApplication.class, args);
+	}
+
+}
